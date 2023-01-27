e243bedebe1c7770aab3c004bf19cd0b09d46a8e
Rami <1040117@mborijnland.nl>
2023-01-27 22:12:21 +0100

    Initial commit

---
+++ partial/loginheader.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/partial.css">
+    <script src="../js/global.js"></script>
+  <script src="https://kit.fontawesome.com/5b96b17e8a.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="../img/logo.png" alt="logo" height="115px">
+        <a href="#" id="openbtn" onclick="openBtn()"><i class="fas fa-bars"></i></a>
+        <nav>
+            <ul>
+                <li><a class="" href="landingspagina.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Registreren</a></li>
+                
+            </ul>
+        </nav>
+    </header>
+
+    <div id="menu" class="menu">
+        <a href="#" class="closebtn" onclick="closeBtn()"><i class="fas fa-times"></i></a>
+        <li><a class="" href="landingspagina.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Registreren</a></li>
+      
+    </div>
+
+</html>
---
+++ partial/mainheader.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/partial.css">
+    <script src="../js/global.js"></script>
+  <script src="https://kit.fontawesome.com/5b96b17e8a.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="../img/logo.png" alt="logo" height="115px">
+        <a href="#" id="openbtn" onclick="openBtn()"><i class="fas fa-bars"></i></a>
+        <nav>
+            <ul>
+                <li><a class="" href="home.php">Blogs</a></li>
+                <li><a href="../backend/logout.php">log uit</a></li>
+                <li><a href="profiel.php">profiel</a></li>
+                <li><a href="blog.php">post</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="menu" class="menu">
+        <a href="#" class="closebtn" onclick="closeBtn()"><i class="fas fa-times"></i></a>
+        <li><a class="" href="home.php">Home</a></li>
+                <li><a href="../backend/logout.php">log uit</a></li>
+      
+    </div>
+
+</html>
