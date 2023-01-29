83fc3a3635d59ac5f5b24adf9d263d0e7be6014f
Rami <74742680+Rami2789@users.noreply.github.com>
2023-01-29 23:31:34 +0100

    Landingspagina aangepast

--- landingspagina.php
+++ landingspagina.php
@@ -1,11 +1,13 @@
 <?php require_once 'partial/loginheader.php';  ?>
+<head>
+        <link rel="stylesheet" href="css/partial.css">
+</head>
+<main style="height: 100vh;">
+<h1>Je bent niet ingelogd</h1>
 
 
-Je bent niet ingelogd
 
-
-
-
+</main>
 
 
 
